84bd143f2d4d312ae333789d4ed8a4fb83a1ae4b
Aakeefah Jones <aakeefahj@gmail.com>
2023-11-12 20:10:41 +0200

    update

--- script/code.js
+++ script/code.js
@@ -1,4 +1,16 @@
 function calculateBMI() {
-    const weight = parseFloat(document.getElementById('weight').value);
-    const height = parseFloat(document.getElementById('height').value);
+    // Get height and weight values from input fields
+    var height = parseFloat(document.getElementById('height').value);
+    var weight = parseFloat(document.getElementById('weight').value);
+
+    // Check if the values are valid numbers
+    if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
+        document.getElementById('result').innerText = 'Please enter valid height and weight.';
+    } else {
+        // Calculate BMI
+        var bmi = weight / Math.pow(height, 2);
+        
+        // Display the result
+        document.getElementById('result').innerText = 'Your BMI is: ' + bmi.toFixed(2);
+    }
 }
